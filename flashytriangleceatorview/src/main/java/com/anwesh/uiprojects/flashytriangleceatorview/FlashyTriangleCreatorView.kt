06ec21d8ec33292318e765bdc813dc90de5592f2
Anwesh43 <anweshthecool0@gmail.com>
2020-06-02 08:23:36 +0530

    adding Canvas extension methods

--- flashytriangleceatorview/src/main/java/com/anwesh/uiprojects/flashytriangleceatorview/FlashyTriangleCreatorView.kt
+++ flashytriangleceatorview/src/main/java/com/anwesh/uiprojects/flashytriangleceatorview/FlashyTriangleCreatorView.kt
@@ -11,6 +11,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.app.Activity
 import android.content.Context
+import android.graphics.PointF
 
 val nodes : Int = 5
 val parts : Int = 3
@@ -20,8 +21,40 @@ val sizeFactor : Float = 2.9f
 val delay : Long = 20
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawFlashyTrianglePart(i : Int, scale : Float, size : Float, paint : Paint) {
+    var sci : Float = scale.divideScale(i, parts)
+    val sfi : Float = sci.sinify()
+    val r : Float = size / rFactor
+    val rsf : Float = r * sfi
+    val verts : Array<Array<Float>> = arrayOf(arrayOf(0f, -size), arrayOf(size, size), arrayOf(-size, size))
+    val iNext : Int = (i + 1) % verts.size
+    val diffNext : PointF = PointF(verts[iNext][0] - verts[i][0], verts[iNext][1] - verts[i][1])
+    save()
+    translate(verts[i][0], verts[i][1])
+    drawCircle(0f, 0f, rsf, paint)
+    drawLine(0f, 0f, diffNext.x * sci, diffNext.y * sci, paint)
+    restore()
+}
+
+fun Canvas.drawFTPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(parts - 1)) {
+        drawFlashyTrianglePart(j, scale, size, paint)
+    }
+    restore()
+}
