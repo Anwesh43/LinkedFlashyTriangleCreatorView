a0bea15aeb010d088a276822827636bef3d06f27
Anwesh43 <anweshthecool0@gmail.com>
2020-06-02 09:10:18 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedflashytrianglecreatorview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedflashytrianglecreatorview/MainActivity.kt
@@ -2,12 +2,19 @@ package com.anwesh.uiprojects.linkedflashytrianglecreatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.flashytriangleceatorview.FlashyTriangleCreatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FlashyTriangleCreatorView.create(this)
+        fullScreen()
     }
 }
 
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
